49e73fb161588d500ecab5a1245f72159f133919
LittleElijah <cuiyeqing960904@gmail.com>
2018-05-24 18:48:48 +0300

    user-tweet relationship

--- app/Tweet.php
+++ app/Tweet.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tweet extends Model
 {
-    //
+  protected $fillable = ['body'];
+
+  public function user()
+  {
+      return $this->belongsTo(User::class);
+  }
+
+  public function scopeLatestFirst($query)
+  {
+      return $query->orderBy('created_at', 'desc');
+  }
 }
